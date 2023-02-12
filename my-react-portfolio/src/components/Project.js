c14ab3a1fb044f4244ad3464649e3c2820d8ffca
Bailey Mejia <baileymejia28@gmail.com>
2023-02-12 00:34:11 -0800

    Added Burnt project photo and the header for the actual project.

--- my-react-portfolio/src/components/Project.js
+++ my-react-portfolio/src/components/Project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import Burnt from '../Assets/Burnt calculator.jpg'
 
 const ProjectHeader = styled.h2`
 display: flex;
@@ -11,10 +11,22 @@ color: #d97706;
 margin-top: 3%;
 `;
 
+const Title = styled.h3`
+font-size: 20px;
+color: black;
+`;
+
+const ProjectTitle = () => {
+    return (
+        <Title>Burnt</Title>
+    )
+}
+
 export default function Project() {
     return (
         <section>
             <ProjectHeader>Portfolio</ProjectHeader>
+            <ProjectTitle />
         </section>
     )
 } 
