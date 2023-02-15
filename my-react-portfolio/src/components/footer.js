77cf668c61965e16e211c438ca01df6e19c66925
Bailey Mejia <baileymejia28@gmail.com>
2023-02-14 17:04:41 -0800

    Added social icons for my github and linkedin.

--- my-react-portfolio/src/components/footer.js
+++ my-react-portfolio/src/components/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 
 const FooterContainer = styled.footer`
@@ -16,11 +17,25 @@ const FooterText = styled.p`
 margin: 0;
 `;
 
+const FontContainer = styled.div`
+display: flex;
+justify-content: start;
+padding: 4%;
+`;
+
+
 const Footer = () => {
     return (
         <FooterContainer>
+            <FontContainer>
+                <a href='https://github.com/BaileyM96'>
+                    <FontAwesomeIcon  icon={faGithub} size="2xl" />
+                </a>
+                <a href='https://www.linkedin.com/in/bailey-mejia-30b055222/'>
+                    <FontAwesomeIcon  icon={faLinkedin} size="2xl" />
+                </a>
+            </FontContainer>
             <FooterText>
-            <FontAwesomeIcon icon="fa-brands fa-github" />
                 Copyright &copy; 2023: Bailey Mejia
             </FooterText>
         </FooterContainer>
